8dbd2871065ff37ec49e0f6fc3b6da7df19ef925
Priscilla Betuyaku <pcgb.com@gmail.com>
2021-04-09 12:10:09 +0100

    First part of the project

---
+++ Xvision/Xtra-vision-kiosk/README.md
@@ -0,0 +1,5 @@
+# Xtra-vision-kiosk
+Assignment: 
+Xtra-vision is a software system that is inside a Video rental kiosk. The Kiosk is placed in the lobby of shopping centres and grocery stores throughout Ireland. The kiosk allows customers to rent movies. Details of this system are available here at this link: https://xtra-vision.ie/how-it-works/
+The Xtra-Vision company has signed a contract with your team to design and write the software for this Kiosk. You will be combining skills from two of the classes that you have this semester, to design and implement a Java program that will provide all of the functionality of the system described in the link above.
+This an integrated assessment between the following year-2 modules: Object Oriented Constructs (OOC) and Systems Analysis and Design (SAAD). For the SAAD class you will do the planning, design, and UML modelling of your proposed system. For the OOC class you will use Object Oriented programming best practices to design and create your proposed system. In other words, you will build a working protype of the system.
---
+++ Xvision/src/Model/Item.java
@@ -0,0 +1,62 @@
+
+package Model;
+
+/**
+ *
+ * @author Betuyaku
+ */
+public class Item {
+    private int idItem;
+    private double price;
+    private int idMovie;
+    private int paymentid;
+    private Category Category;
+    
+    public Item(Category Category){
+        this.Category = Category;
+    }
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getIdMovie() {
+        return idMovie;
+    }
+
+    public void setIdMovie(int idMovie) {
+        this.idMovie = idMovie;
+    }
+
+    public int getPaymentid() {
+        return paymentid;
+    }
+
+    public void setPaymentid(int paymentid) {
+        this.paymentid = paymentid;
+    }
+
+    public Category getCategory() {
+        return Category;
+    }
+
+    public void setCategory(Category Category) {
+        this.Category = Category;
+    }
+
+
+    
+    
+}
